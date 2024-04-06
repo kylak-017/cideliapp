518001a05a1015a4a0b3f6f4f641e05d8b188002
kylak017 <KyuriKim017@gmail.com>
2024-04-06 19:13:24 +0900

    changes uploaded

--- menu/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
-
-def index(request):
-    return HttpResponse("Hello, welcome to CI Deli Pre-Order System.") #getting an html page
-
-# Create your views here.
